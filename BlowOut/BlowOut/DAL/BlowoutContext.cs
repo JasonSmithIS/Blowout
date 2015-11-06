da272b4496190db6a995a8e2e6b945be59fcd276
Jason <JasonSmithIS@users.noreply.github.com>
2015-11-06 12:41:37 -0700

    DB Context & Set
    I have an error on the Db context, becuase I'm not sure how to reference
    the models

---
+++ BlowOut/BlowOut/DAL/BlowoutContext.cs
@@ -0,0 +1,19 @@
+﻿using BlowOut.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.DAL
+{
+    public class BlowOutContext : DbContext
+    {
+        public BlowOutContext() : base("BlowOutContext") 
+        {
+
+        }
+
+        public DbSet<BlowOut> BlowOuts { get; set; }
+    }
+}
