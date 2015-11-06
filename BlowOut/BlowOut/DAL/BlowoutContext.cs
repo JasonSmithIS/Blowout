fa799260e3e4e4e9801b44790bf239668646f863
Jason <JasonSmithIS@users.noreply.github.com>
2015-11-06 12:43:18 -0700

    DB Context Fixed

--- BlowOut/BlowOut/DAL/BlowoutContext.cs
+++ BlowOut/BlowOut/DAL/BlowoutContext.cs
@@ -14,6 +14,7 @@ namespace BlowOut.DAL
 
         }
 
-        public DbSet<BlowOut> BlowOuts { get; set; }
+        public DbSet<Client> BlowOuts { get; set; }
+        public DbSet<Instrument> BlowOuts { get; set; }
     }
 }
