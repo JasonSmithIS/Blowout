433f01fa7bf79686fadf058b837e178a0c532d2c
Jason <JasonSmithIS@users.noreply.github.com>
2015-11-06 12:17:18 -0700

    Double Class
    One Model with two classes

--- BlowOut/BlowOut/Models/BlowOut.cs
+++ BlowOut/BlowOut/Models/BlowOut.cs
@@ -12,10 +12,24 @@ namespace BlowOut.Models
     {
         [Key]
         public int ClientID { get; set; }
+        public string ClientFirstName { get; set; }
+        public string ClientLastName { get; set; }
+        public string ClientAddress { get; set; }
+        public string ClientCity { get; set; }
+        public string ClientState { get; set; }
+        public string ClientZip { get; set; }
+        public string ClientEmail { get; set; }
+        public string ClientPhone { get; set; }
     }
     [Table("Instrument")]
     public class Instrument 
     { 
         [Key]
+        public int InstrumentID { get; set; }
+        public string InstrumentDesc { get; set; }
+        public int ClientID { get; set; }
+        public string InstrumentImg { get; set; }
+        public double InstrumentPrice { get; set; }
+        public string InstrumentType { get; set; }
     }
 }
